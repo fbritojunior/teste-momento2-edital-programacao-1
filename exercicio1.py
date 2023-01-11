4413d344095dc86e7677f10f754de47136a4afe8
Francisco de Brito Jr <fbritojnr@gmail.com>
2023-01-11 15:31:41 -0300

    Update exercicio1.py

--- exercicio1.py
+++ exercicio1.py
@@ -3,34 +3,25 @@ Faça um programa que preencha um vetor com 6 valores distintos digitados pelo u
 Em seguida, exiba o maior e o menor valor do vetor, indicando em qual posição eles se encontram. 
 Depois, imprima os itens no vetor em ordem crescente.
 """
-import numpy as np
 
-vet = np.array(np.zeros(6))
+print('Digite os 06 números para preencher o vetor:')
+vet = []
 
-print("Nota: Devem ser digitados seis (06) valores inteiros ou com (.) como separação decimal.")
+n = 6 
+  
+for i in range(0, n):
+    elem = float(input())
+    vet.append(elem)
 
-while True:
-    try:
-        for e in range(6):
-            print(f"Digite o valor do {e + 1}º elemento: ")
-            vet[e] = input()
-        break
-    except ValueError:
-        print("Erro! Digite números válidos.")
-        continue
+print('Fim do vetor. \n')
 
-print(vet)
+min_val = min(vet)
+pos_min = vet.index(min_val)
+max_val = max(vet)
+pos_max = vet.index(max_val)
+lst_ord = sorted(vet)
 
-min_val = np.min(vet)
-min_index = np.argmin(vet) + 1
-print(f"\nO valor mínimo encontrado no vetor é {min_val} na posição {min_index}.")
-
-max_val = np.max(vet)
-max_index = np.argmax(vet) + 1
-print(f"O valor máximmo encontrado no vetor é {max_val} na posição {max_index}.")
-
-orr_vet =  np.sort(vet)
-print("\nO vetor ordenado:")
-print(orr_vet)
-
+print(f'O menor valor do vetor é {min_val}, encontrado na posição {pos_min}.')
+print(f'O maior valor do vetor é {max_val}, encontrado na posição {pos_max}.')
+print('Itens no vetor em ordem crescente:', lst_ord)
  
