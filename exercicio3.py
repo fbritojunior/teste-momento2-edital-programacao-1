a4be042bc9bcca95af930e4489ad39ac4a7f7f3f
Francisco de Brito Jr <fbritojnr@gmail.com>
2023-01-11 15:33:52 -0300

    Update exercicio3.py

--- exercicio3.py
+++ exercicio3.py
@@ -3,30 +3,38 @@ Escreva um programa que lê 2 matrizes A e B, cada uma com 3 linhas e 2 colunas.
 Construir uma matriz C de mesma dimensão (3x2) onde C é formada pela soma dos elementos da matriz A com os elementos da matriz B 
 (exemplo: C[1][1] = A[1][1] + B[1][1]). Apresentar ao final as 3 matrizes (A, B e C).
 """
-import numpy as np 
 
-A = np.array(np.zeros((3, 2)))
-B = np.array(np.zeros((3, 2)))
-C = np.array(np.zeros((3, 2)))
+A = []
+B = []
+C = []
+r = 3
+c = 2
+ 
+print('Digite os elementos da matriz A:')
+for i in range(0, r):
+	itens = []
+	for j in range (0, c):
+		itens.append(float(input()))
 
-x, y = np.shape(C)
+	A.append(itens)
+print('Fim da matriz. \n')
 
-print(f"Digite a matriz A. ")
-for l in range(x):
-    for c in range(y):
-        print(f"Digite o valor do elemento na linha {l+1} e coluna {c+1}: ")
-        A[l, c] = input()
+print('Digite os elementos da matriz B:')
+for i in range(0, r):
+	itens = []
+	for j in range (0, c):
+		itens.append(float(input()))
 
-print(f"\nDigite a matriz B. ")
-for l in range(x):
-    for c in range(y):
-        print(f"Digite o valor do elemento na linha {l+1} e coluna {c+1}: ")
-        B[l, c] = input()
+	B.append(itens)
+print('Fim da matriz. \n')
 
-for l in range(x):
-    for c in range(y):
-        C[l, c] = A[l, c] + B[l, c]
+for i in range(0, r):
+	itens = []
+	for j in range (0, c):
+		itens.append(A[i][j]+B[i][j])
 
-print("A matriz A é: \n",A)
-print("A matriz B é: \n",B)
-print("A matriz C = A + B é: \n",C)
+	C.append(itens)
+
+print('A matriz A: ', A)
+print('A matriz B: ', B)
+print('A matriz C = A + B: ', C)
